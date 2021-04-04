499fcd8896e2728b0787fe2eed002ad546db64bc
cristiano7988 <cristianooliveiramorales@gmail.com>
2021-04-03 23:12:55 -0300

    Refatoração utilizando Material UI no Editor

--- resources/js/components/Editor/Editor.jsx
+++ resources/js/components/Editor/Editor.jsx
@@ -1,104 +1,105 @@
+import { Card, Container, Icon, Typography } from '@material-ui/core';
 import React, { Component } from 'react';
 import Braco from '../Braco';
 
 class Editor extends Component {
-    constructor() {
-        super();
-        this.afinar = this.afinar.bind(this);
-        this.remover = this.remover.bind(this);
-        this.adicionar = this.adicionar.bind(this);
-        this.estender = this.estender.bind(this);
-        this.state = {
-            contador: [{
-                cordas: []
-            }],
-            afinar: false,
-            estender: false,
-        }
+  constructor() {
+    super();
+    this.afinar = this.afinar.bind(this);
+    this.remover = this.remover.bind(this);
+    this.adicionar = this.adicionar.bind(this);
+    this.estender = this.estender.bind(this);
+    this.state = {
+      contador: [{
+        cordas: []
+      }],
+      afinar: false,
+      estender: false,
     }
+  }
 
-    estender(e) {
-        e.preventDefault();
-        e.target.classList.toggle('estender')
-        this.setState({estender: e.target.classList.contains('estender')})
+  estender(e) {
+    e.preventDefault();
+    e.target.classList.toggle('estender')
+    this.setState({estender: e.target.classList.contains('estender')})
+  }
+
+  afinar(e) {
+    e.preventDefault();
+    e.target.classList.toggle('afinar')
+    this.setState({afinar: e.target.classList.contains('afinar')})
+  }
+
+  remover(e) {
+    e.preventDefault();
+    let contador = this.state.contador;
+    if(contador.length > 1) {
+      contador.pop();
+      this.setState(contador);
     }
+  }
 
-    afinar(e) {
-        e.preventDefault();
-        e.target.classList.toggle('afinar')
-        this.setState({afinar: e.target.classList.contains('afinar')})
-    }
+  adicionar(e) {
+    e.preventDefault();
+    const contador = this.state.contador;
+    contador.push({ cordas: [] });
+    this.setState(contador);
+  }
 
-    remover(e) {
-        e.preventDefault();
-        let contador = this.state.contador;
-        if(contador.length > 1) {
-            contador.pop();
-            this.setState(contador);
-        }
-    }
-
-    adicionar(e) {
-        e.preventDefault();
-        const contador = this.state.contador;
-        contador.push({ cordas: [] });
-        this.setState(contador);
-    }
-
-    render() {
-        return (
-            <div className="container py-4">
-                <div className="row justify-content-center">
-                    <div className="col-md-12">
-                        <div className="card text-center">
-                            <h1 className="m-5">Editor</h1>
-                            <h2 className="mb-5">Cifras</h2>
-                            <div className="row m-auto pb-5">
-                                {this.state.contador.map((braco, id)=>{
-                                    return (
-                                        <div key={id} className="container-braco">
-                                            <Braco
-                                                id={id}
-                                                braco={braco}
-                                                escala={false}
-                                                digitar={true}
-                                                afinar={this.state.afinar}
-                                                estender={this.state.estender}
-                                            />
-                                        </div>
-                                    )
-                                })}
-                                <div>
-                                    <div className="container-btn">
-                                        <span
-                                            title="Mudar tamanho do braço"
-                                            className="btn-editor arm"
-                                            onClick={this.estender}
-                                        />
-                                        <span
-                                            title="Habilitar Afinação"
-                                            className="btn-editor hand"
-                                            onClick={this.afinar}
-                                        />
-                                        <span
-                                            title="Remover braço"
-                                            className="btn-editor"
-                                            onClick={this.remover}
-                                        >-</span>
-                                        <span
-                                            title="Adicionar braço"
-                                            className="btn-editor"
-                                            onClick={this.adicionar}
-                                        >+</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <Container maxWidth="md" align="center" className="editor">
+        <Card className="folder">
+          <Typography
+            component="h1"
+            variant="h4"
+            children="Editor"
+            gutterBottom
+          />
+          <Typography
+            component="h2"
+            variant="h5"
+            children="Cifras"
+            gutterBottom
+          />
+          {this.state.contador.map((braco, id) => (
+            <div key={id} className="container-braco">
+              <Braco
+                id={id}
+                braco={braco}
+                escala={false}
+                digitar={true}
+                afinar={this.state.afinar}
+                estender={this.state.estender}
+              />
             </div>
-         );
-    }
+          ))}
+          <div className="container-btn">
+            <span
+              title="Mudar tamanho do braço"
+              className="btn-editor arm"
+              onClick={this.estender}
+            />
+            <span
+              title="Habilitar Afinação"
+              className="btn-editor hand"
+              onClick={this.afinar}
+            />
+            <span
+              title="Remover braço"
+              className="btn-editor"
+              onClick={this.remover}
+            >-</span>
+            <span
+              title="Adicionar braço"
+              className="btn-editor"
+              onClick={this.adicionar}
+            >+</span>
+          </div>
+        </Card>
+      </Container>
+    );
+  }
 }
  
 export default Editor;
